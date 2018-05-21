b258574a478b058d4445c479a31b1e3148ae9d42
Anurag <anuragphadke19@gmail.com>
2018-05-20 22:07:19 -0400

    Modified the blockchain to check for hacks

--- blockchain.py
+++ blockchain.py
@@ -32,13 +32,41 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting block')
         print(block)
+    else:
+        print('-' * 20)
 
 
+def verify_blockchain():
+    # Verify that the blockchain has not been manipulated
+    is_valid = True
+    #block_index = 0
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    """ for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1 """
+    return is_valid
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose :')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the blockchain')
     print('q: Quit')
     
     user_choice = get_user_choice()
@@ -47,11 +75,20 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >=1:
+            blockchain[0] = [[2]]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     print('Choice registered!')
+    if not verify_blockchain():
+        print_blockchain_elements()
+        print("You have been hacked. The blockchain is invalid now.")
+        break
+else:
+    print("Peace out!!")
 
 
 print('Done!')
