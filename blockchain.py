324c6aa1bd21f7480a60c207ceee90c9449e3f2d
Anurag <anuragphadke19@gmail.com>
2018-05-31 07:12:01 -0400

    modified the hashing logic and added a set for participants list. We have a verifiable blockchain now

--- blockchain.py
+++ blockchain.py
@@ -7,12 +7,18 @@ genesis_block={
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Neo'
+participants = {owner}
+
 
 def get_last_blockchain_value():
     ''' Returns the lst blockchain value '''
     if len(blockchain) < 1:
         return None
     return blockchain[-1]
+
+
+def hash_block(block):
+    return '-'.join(str(block[key]) for key in block)
 
 
 def add_transaction(reciever, sender=owner, amount=1.0):
@@ -28,14 +34,14 @@ def add_transaction(reciever, sender=owner, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(reciever)
 
 
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = ''
-    for key in last_block:
-        value = last_block[key]
-        hashed_block = hashed_block + str(value)
+    hashed_block = hash_block(last_block)
+    
     print(hashed_block)
     block = {
         'previous_hash': hashed_block,
@@ -67,27 +73,12 @@ def print_blockchain_elements():
 
 def verify_blockchain():
     # Verify that the blockchain has not been manipulated
-    is_valid = True
-    #block_index = 0
-    for block_index in range(len(blockchain)):
-        if block_index == 0:
+    for (index,block) in enumerate(blockchain):
+        if index == 0:
             continue
-        elif blockchain[block_index][0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-    """ for block in blockchain:
-        if block_index == 0:
-            block_index += 1
-            continue
-        elif block[0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-        block_index += 1 """
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 waiting_for_input = True
 
@@ -96,6 +87,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a block')
     print('3: Output the blockchain blocks')
+    print('4: Output the participants')
     print('h: Manipulate the blockchain')
     print('q: Quit')
     
@@ -109,18 +101,24 @@ while waiting_for_input:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         if len(blockchain) >=1:
-            blockchain[0] = [[2]]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [{'sender': 'Tom', 'reciever':'Dick', 'amount': 100}]
+            }
     elif user_choice == 'q':
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     print('Choice registered!')
-    # if not verify_blockchain():
-    #     print_blockchain_elements()
-    #     print("You have been hacked. The blockchain is invalid now.")
-    #     break
+    if not verify_blockchain():
+        print_blockchain_elements()
+        print("You have been hacked. The blockchain is invalid now.")
+        break
 else:
     print("Peace out!!")
 
